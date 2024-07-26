18fca4b6182523ba16269029cda72e84b915b002
Lingesh <118330035+lingesh0@users.noreply.github.com>
2024-07-26 09:49:33 +0530

    Add files via upload

---
+++ ski-login/src/App.js
@@ -0,0 +1,13 @@
+// App.js
+import React from 'react';
+import Login1 from './components/Login1';
+
+function App() {
+  return (
+    <div className="App">
+      <Login1 />
+    </div>
+  );
+}
+
+export default App;
